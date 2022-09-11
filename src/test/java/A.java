365fd2d0eed5e2e6861fd257896b5c1e303dc0f5
91797 <91797@DESKTOP-T38U104>
2022-09-11 09:30:30 +0530

    it is my first comment on github

---
+++ src/test/java/A.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class A {
+public static void main(String[] args) {
+	WebDriver driver=WebDriverManager.chromedriver().create();
+	driver.get("https://www.shaadi.com/");
+	
+}
+}
