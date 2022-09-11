ddd3f4f72953fd8deffc3a128d84675f7ba1fccb
AjijTCS <113273542+AjijTCS@users.noreply.github.com>
2022-09-11 09:43:27 +0530

    Update A.java

--- src/test/java/A.java
+++ src/test/java/A.java
@@ -6,6 +6,6 @@ public class A {
 public static void main(String[] args) {
 	WebDriver driver=WebDriverManager.chromedriver().create();
 	driver.get("https://www.shaadi.com/");
-	
+	System.out.println("Ajij");
 }
 }
